0824c9f0a084f5bd70e61df0b8b6dce16c1050be
Abdul Rahman <116431477+Abdul-Rahman-9040@users.noreply.github.com>
2023-12-23 20:35:30 +0530

    Add files via upload

---
+++ javascript/main_script.js
@@ -0,0 +1,10 @@
+var navLinks= document.getElementById("navLinks");
+
+function showmenu(){
+    navLinks.style.left="0";
+}   
+function hidemenu(){
+    navLinks.style.left="-200px";
+}
+
+// document.addEventListener('contextmenu', event => event.preventDefault());
